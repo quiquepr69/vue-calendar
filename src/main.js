7a69b57cc616e3a8cfdaf876649eef6f9de7f01f
Carlos Montalvo <carlos@carlos-montalvo.com>
2019-12-02 19:59:21 -0500

    calendar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCvai9XOxbuxhcmLOsglY48azFn04yduc0",
+  authDomain: "vue-calendar-80e6f.firebaseapp.com",
+  databaseURL: "https://vue-calendar-80e6f.firebaseio.com",
+  projectId: "vue-calendar-80e6f",
+  storageBucket: "vue-calendar-80e6f.appspot.com",
+  messagingSenderId: "969164038491",
+  appId: "1:969164038491:web:16e227070196bff869ba7e"
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
